b5a6987dae8c6244e9ad8444d9b7d3ea17acd4c6
Leonardo Gideão Costa Rocha <gideonveloz@gmail.com>
2017-06-16 23:28:13 -0300

    Add files via upload

---
+++ www/js/handleBars.js
@@ -0,0 +1,10 @@
+function handleBarsCarregarTratores(opcoesT){
+  var source = $("#lista-opcoes-template").html();
+  var template = Handlebars.compile(source);
+  $("#more-information").append(template(opcoesT));
+}
+function handleBarsMostrarTratores(listaTratores){
+	var source = $('#lista-tratores-template').html();
+	var template = Handlebars.compile(source);
+	$("#box-tratores").append(template(listaTratores));
+}
---
+++ www/js/opcaoTrator.js
@@ -0,0 +1,6 @@
+var opcaoT = function(componenteTitulo,grauImportancia,id){
+  this.componenteTitulo = componenteTitulo;
+  this.grauImportancia = grauImportancia;
+  this.id = id;
+  this.possui = true;
+}
