6eda6a6fd422018ce4348c5b168c0514d53bfa60
Leonardo Gideão Costa Rocha <gideonveloz@gmail.com>
2017-06-16 23:24:14 -0300

    Delete opcaoTrator.js

--- www/js/opcaoTrator.js
+++
@@ -1,6 +0,0 @@
-var opcaoT = function(componenteTitulo,grauImportancia,id){
-  this.componenteTitulo = componenteTitulo;
-  this.grauImportancia = grauImportancia;
-  this.id = id;
-  this.possui = true;
-}
